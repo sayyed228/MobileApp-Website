0da466edf9f17b4ca165fd6d49c345e31f35cb08
sayyed228 <nazirahmed230@gmail.com>
2015-09-04 20:26:46 +0530

    version 2.3

--- demo1/js/custom.js
+++ demo1/js/custom.js
@@ -1,7 +1,43 @@
 "use strict";
 
+
+
 $(document).ready(function() {
+	
+
+	
+$('#myCarouselTwo').carousel({
+    interval: 7000
+});
+
+
+$('.carousel-indicators li').click(function(){
+  $(this).removeClass('hvr-float-shadow');
+}); 
+
+	
   var winHeight = $(window).height();	
-		//alert(winHeight)
-		$('.home').height(winHeight);
- });
+	$('.home').height(winHeight);
+  });
+ 
+ 
+
+
+
+var display_backtotop = 200; // Scrolled pixels from top until the link appears
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > display_backtotop) {
+      $(".scroll-top").fadeIn();
+    } else {
+      $(".scroll-top").fadeOut();
+    }
+  });
+  
+  $(document).on("click", ".scroll-top", function(e) {
+    $("html, body").animate({scrollTop: 0}, 1500);
+    
+    e.preventDefault();
+  });
+});
